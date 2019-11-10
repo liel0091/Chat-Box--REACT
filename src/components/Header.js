f4cf197fbebcfde9479c30b80d4c31e2835af1a6
liel0978 <linn.eliassen@telia.com>
2019-11-10 12:18:01 +0100

    header fin lir

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,5 @@
 import React, {useState}  from 'react';  
-import { useSpring, animated as a } from 'react-spring'
+import {useSpring, animated as a} from 'react-spring'
 import '../style/header.css'
  
 function Header() { 
@@ -8,14 +8,14 @@ function Header() {
     const { transform, opacity } = useSpring({
       opacity: flipped ? 1 : 0,
       transform: `perspective(600px) rotateX(${flipped ? 180 : 0}deg)`,
-      config: { mass: 5, tension: 500, friction: 80 }
+      config: {mass: 5, tension: 500, friction: 80}
     })
 
     return (   
         <div className="header white-border" onClick={() => set(state => !state)}>
-            <a.div className="h-img back" style={{ opacity: opacity.interpolate(o => 1 - o), transform }} />
+            <a.div className="h-img back" style={{ opacity: opacity.interpolate(o => 1 - o), transform }}/>
             <h1>Spooky Headline</h1>
-            <a.div className="h-img front" style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`) }} />
+            <a.div className="h-img front" style={{ opacity, transform: transform.interpolate(t => `${t} rotateX(180deg)`)}}/>
         </div> 
     )
 }
